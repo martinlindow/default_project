4ec7be85418e7378e8800b942bf84c69c52d9713
Martin Lindow <martin.lindow@googlemail.com>
2018-04-13 22:14:44 +0200

    Initial commit

---
+++ backend/src/main/java/de/lindow_it/backend/App.java
@@ -0,0 +1,16 @@
+package de.lindow_it.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Class
+ * @author privat
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+    	SpringApplication.run(App.class, args);
+    }
+}
